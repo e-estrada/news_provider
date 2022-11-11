5f7a84724669ecb1fa5296279849f25462dd691d
Erick Estrada <eof105@gmail.com>
2022-11-10 20:29:20 -0600

    Inicio de proyecto - NewsProvider

---
+++ lib/src/pages/tabs_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TabsPage extends StatelessWidget {
+   
+  const TabsPage({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: [
+          Container(
+            color: Colors.red,
+          ),
+
+          Container(
+            color: Colors.green,
+          )
+        ],
+      )
+    );
+  }
+}
