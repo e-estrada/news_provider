7daac6ab7efb43a75d5c9ea4d64d619a46a27f85
Erick Estrada <eof105@gmail.com>
2022-11-11 16:10:39 -0600

    Diseño del widget de las noticias

--- lib/src/widgets/lista_noticias.dart
+++ lib/src/widgets/lista_noticias.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_provider/src/models/news_models.dart';
+import 'package:news_provider/src/theme/tema.dart';
 
 class ListaNoticias extends StatelessWidget {
   const ListaNoticias({super.key, required this.noticias});
@@ -7,6 +8,130 @@ class ListaNoticias extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemCount: noticias.length, itemBuilder: (context, index) => Text(noticias[index].title));
+    return ListView.builder(
+        itemCount: noticias.length,
+        itemBuilder: (context, index) {
+          return _Noticia(noticia: noticias[index], index: index);
+        });
+  }
+}
+
+class _Noticia extends StatelessWidget {
+  const _Noticia({required this.noticia, required this.index});
+  final Article noticia;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _TarjetaTopBar(
+          noticia: noticia,
+          index: index,
+        ),
+        _TarjetaTitulo(noticia: noticia),
+        _TarjetaImagen(noticia: noticia),
+        _TarjetaBody(noticia: noticia),
+
+        const SizedBox(height: 10),
+        const Divider(),
+
+        _TarjetaBotones(),
+      ],
+    );
+  }
+}
+
+class _TarjetaTopBar extends StatelessWidget {
+  final Article noticia;
+  final int index;
+
+  const _TarjetaTopBar({required this.noticia, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      margin: const EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: [
+          Text('${index + 1} ', style: TextStyle(color: miTema.colorScheme.secondary)),
+          Text('${noticia.source.name} '),
+        ],
+      ),
+    );
+  }
+}
+
+class _TarjetaTitulo extends StatelessWidget {
+  final Article noticia;
+  const _TarjetaTitulo({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Text(noticia.title, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w700)),
+    );
+  }
+}
+
+class _TarjetaImagen extends StatelessWidget {
+  final Article noticia;
+  const _TarjetaImagen({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(topLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
+        child: Container(
+            child: (noticia.urlToImage != null && noticia.urlToImage!.contains('http'))
+                ? FadeInImage(placeholder: const AssetImage('assets/img/giphy.gif'), image: NetworkImage(noticia.urlToImage!))
+                : const Image(image: AssetImage('assets/img/no-image.png'))),
+      ),
+    );
+  }
+}
+
+class _TarjetaBody extends StatelessWidget {
+  final Article noticia;
+  const _TarjetaBody({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Text(noticia.description ?? ''),
+    );
+  }
+}
+
+class _TarjetaBotones extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RawMaterialButton(
+          onPressed: () {
+            
+          },
+          fillColor: miTema.colorScheme.secondary,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: const Icon(Icons.star_border),
+        ),
+
+        const SizedBox(width: 10),
+
+        RawMaterialButton(
+          onPressed: () {},
+          fillColor: Colors.blue,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: const Icon(Icons.more),
+        ),
+      ],
+    );
   }
 }
